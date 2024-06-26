73dccf379fa8c37516f88b2fd551a6737c329e8d
Prasadj21072003 <prasadjadhav2003@gmail.com>
2024-06-26 16:46:12 +0530

    eighth commit

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const PORT: Number | String = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://zippy-sprite-4f4938.netlify.app",
     credentials: true,
   })
 );
